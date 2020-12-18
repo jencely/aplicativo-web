425851c6d7d49278085d36b2eb6edd223f04c994
jencely <jeny.guanuna@gmail.com>
2020-12-18 01:20:12 -0500

    aplicativo web mvc

---
+++ Agro.Rop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Agro.Rop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
